296b73bbb24ce6254c80b47936f2a705514bf690
Marika <marikabartman27@gmail.com>
2025-04-30 11:39:28 +0100

    3 kata finished

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,13 +48,51 @@ describe("GET /api/topics", () => {
         expect(Array.isArray(topics)).toBe(true);
         expect(topics.length).toBe(3);
         expect(topics[0] instanceof Object).toBe(true);
-        topics.forEach((topic) =>{
+        topics.forEach((topic) => {
           expect(topic).toMatchObject({
             slug: expect.any(String),
             description: expect.any(String),
-            
           });
-        })
+        });
       });
   });
+});
+describe("GET article by id", () => {
+  test("200: responds with article by id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("400- Bad request when passed an ivalid article_id", () => {
+    return request(app)
+      .get("/api/articles/notNum")
+      .expect(400)
+      .then(({body: {msg}}) => {
+       
+        expect(msg).toBe("Bad request");
+      });
+  });
+  test("404: article_id does not exist", ()=>{
+    return request(app)
+    .get("/api/articles/1000")
+    .expect(404)
+    .then(({body: {msg}}) => {
+      
+      expect(msg).toBe("article id is not found");
+    });
+  }
+  )
 });
--- listen.js
+++ listen.js
@@ -3,3 +3,14 @@ const app = require("./api")
 app.listen(9090, ()=>{
     console.log("app listening on port 9090")
 })
+
+const express = require("express");
+// const app = express();
+
+// // Middleware i endpointy tutaj
+
+// app.listen(3000, () => {
+//     console.log("Serwer działa na porcie 3000!");
+// });
+
+module.exports = app;
