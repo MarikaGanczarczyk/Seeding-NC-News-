fd0254539d39d002c044d55c63708a54175f3900
Marika <marikabartman27@gmail.com>
2025-05-07 19:13:12 +0100

    12 task get articles by id with comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -410,8 +410,8 @@ describe("GET /api/articles (topic query)", () => {
       .expect(200)
       .then((response) => {
         const articles = response.body.articles;
-     console.log(articles, " <----");
-     
+      
+
         articles.forEach((article) => {
           expect(article).toHaveProperty("article_id");
           expect(article).toHaveProperty("topic");
@@ -421,6 +421,86 @@ describe("GET /api/articles (topic query)", () => {
           expect(article).toHaveProperty("article_img_url");
           expect(article.topic).toBe("mitch");
         });
+      });
+  });
+  test("200 : respond with an empty array if there is no valid topic", ()=>{
+    return request(app)
+    .get("/api/articles?topic=paper")
+    .expect(200)
+    .then(response =>{
+      expect(response.body.articles).toEqual([])
+    })
+  })
+});
+describe("GET /api/articles (topic query) - error handling", ()=>{
+  test("400: responds with an error when bad request", ()=>{
+   return request(app)
+     .get("/api/articles/topic=flower")
+     .expect(400)
+     .then(response =>{
+       expect(response.body.msg).toEqual("Bad request")
+     })
+  })
+ })
+
+
+
+
+describe("GET /api/articles/:article_id (comment count", () => {
+  test("200 : responds with article with comment count", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        const article = response.body.article;
+        expect(article.title).toBe("Living in the shadow of a great man"),
+          expect(article.topic).toBe("mitch");
+        expect(article.author).toBe("butter_bridge");
+        expect(article.body).toBe("I find this existence challenging");
+        expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(article.votes).toBe(100);
+
+        expect(article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+        expect(article.comment_count).toBe(11);
+      });
+  });
+  test("200 : responds with article with no comment count", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .expect(200)
+      .then((response) => {
+        const article = response.body.article;
+        expect(article.title).toBe("Sony Vaio; or, The Laptop"),
+          expect(article.topic).toBe("mitch");
+        expect(article.author).toBe("icellusedkars");
+        expect(article.body).toBe("Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.");
+        expect(article.created_at).toBe("2020-10-16T05:03:00.000Z");
+        expect(article.votes).toBe(0);
+
+        expect(article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+        expect(article.comment_count).toBe(0);
+      });
+  });
+  test("404: responds with Not found if article_id doesn't exist", () => {
+    return request(app)
+      .get("/api/articles/1000")
+      
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("article id is not found");
+      });
+  });
+  test("400: responds with bad request if invalid article_id", () => {
+    return request(app)
+      .get("/api/articles/notNum")
+      
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
       });
   });
 });
